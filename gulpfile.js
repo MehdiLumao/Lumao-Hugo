9516a1278b70622cd40d067b074d06033b5d543b
Aurélien Lavorel <aurelien@lavoweb.net>
2016-12-24 12:15:54 +0100

    [update] migrate to hugo

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('css', function() {
-    return gulp.src('./css/*.css')
+    return gulp.src('./themes/lumeo/static/css/*.css')
         .pipe(concat('all.css'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./themes/lumeo/static/dist/'));
 });
 
 gulp.task('js', function() {
-    return gulp.src(['./js/first/*.js', './js/*.js'])
+    return gulp.src(['./themes/lumeo/static/js/first/*.js', './themes/lumeo/static/js/*.js'])
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./themes/lumeo/static/dist/'));
 });
