55a5e66a81f8e00e5477c6ad7ca17b849eb36fbf
Aurélien Lavorel <aurelien@lavoweb.net>
2017-11-28 16:30:03 +0100

    [fix] typo

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('css', function() {
-    return gulp.src('./themes/lumeo/static/css/*.css')
+    return gulp.src('./themes/lumao/static/css/*.css')
         .pipe(concat('all.css'))
-        .pipe(gulp.dest('./themes/lumeo/static/dist/'));
+        .pipe(gulp.dest('./themes/lumao/static/dist/'));
 });
 
 gulp.task('js', function() {
-    return gulp.src(['./themes/lumeo/static/js/first/*.js', './themes/lumeo/static/js/*.js'])
+    return gulp.src(['./themes/lumao/static/js/first/*.js', './themes/lumao/static/js/*.js'])
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('./themes/lumeo/static/dist/'));
+        .pipe(gulp.dest('./themes/lumao/static/dist/'));
 });
