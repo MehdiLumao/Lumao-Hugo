bb6267c6305d634262c24547e812a8412116d2d8
julien <julien@lumao.eu>
2021-02-11 18:25:53 +0100

    Home hero block

--- themes/next/assets/css/tailwind.config.js
+++ themes/next/assets/css/tailwind.config.js
@@ -3,6 +3,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        primary: '#ff9c00',
+        'transparent-black': 'rgba(0, 0, 0, 0.5)'
+      },
+      minHeight: {
+        '600': '600px'
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       }
@@ -10,6 +17,9 @@ module.exports = {
   },
   variants: {
     extend: {
+      display: ['group-hover'],
+      margin: ['hover'],
+      borderWidth: ['hover']
     }
   },
   plugins: [
