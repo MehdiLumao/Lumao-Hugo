aada1a7252bafe3c840a276320d11cb457d31b28
julien <julien@lumao.eu>
2021-02-18 16:19:45 +0100

    Footer

--- themes/next/assets/css/tailwind.config.js
+++ themes/next/assets/css/tailwind.config.js
@@ -13,7 +13,14 @@ module.exports = {
         'transparent-black': 'rgba(0, 0, 0, 0.5)',
         'gradient-dark': '#0098c7',
         'gradient-light': '#0de9c9',
-        gray: colors.trueGray
+        gray: colors.trueGray,
+        'gray-theme': {
+          900: '#181818',
+          800: '#202020',
+          750: '#242424',
+          700: '#2b2b2b',
+          600: '#3e3e3e'
+        }
       },
       height: {
         '600': '600px'
